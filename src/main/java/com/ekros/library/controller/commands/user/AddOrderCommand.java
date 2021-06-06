56f80ba9b312c886a83aae72f577de2cd35fa8da
Ekrosrb <Ekrosrb@gmail.com>
2021-06-06 22:08:18 +0300

    order exist message | locale dataset fix

--- src/main/java/com/ekros/library/controller/commands/user/AddOrderCommand.java
+++ src/main/java/com/ekros/library/controller/commands/user/AddOrderCommand.java
@@ -6,13 +6,17 @@ import com.ekros.library.controller.commands.Path;
 import com.ekros.library.model.entity.Status;
 import com.ekros.library.model.entity.Order;
 import com.ekros.library.model.service.OrderService;
+import org.apache.log4j.Logger;
 
 import javax.servlet.http.HttpServletRequest;
 import java.sql.Date;
+import java.sql.SQLException;
 
 public class AddOrderCommand implements ICommand {
 
     private final OrderService orderService;
+
+    private final Logger log = Logger.getLogger(AddOrderCommand.class);
 
     public AddOrderCommand(OrderService orderService) {
         this.orderService = orderService;
@@ -28,9 +32,15 @@ public class AddOrderCommand implements ICommand {
             CommandUtils.setMessage(request, "Wrong request!");
             return Path.ERROR_PAGE;
         }
-
-        Order sub = new Order(Integer.parseInt(userId), Integer.parseInt(bookId), term, null, 0L, Status.PENDING);
-        orderService.addOrder(sub);
+        try {
+            orderService.addOrder(new Order(Integer.parseInt(userId),
+                    Integer.parseInt(bookId),
+                    term, null, 0L, Status.PENDING));
+        }catch (SQLException e){
+            log.error(e.getMessage());
+            CommandUtils.setMessage(request, "You already have an active order for this book.");
+            return Path.MAIN_PAGE;
+        }
         return Path.REDIRECT_PROFILE;
     }
 }
